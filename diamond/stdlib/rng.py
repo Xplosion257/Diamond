819a5749194f8443ea67e571c800542a9cfdecea
Xplosion257 <eric.jiyu.wang@gmail.com>
2025-11-30 09:39:59 -0500

    New additions to the rng library!

--- diamond/stdlib/rng.py
+++ diamond/stdlib/rng.py
@@ -25,3 +25,41 @@ def chance(percent):
 # Random float
 def random_float():
     return random.random()
+
+# Random choice
+def pick(list):
+    return random.choice(list)
+
+# Weighted choice
+def weight(population, weights=None, k=1):
+    random.choices(population, weights, k)
+
+# Unique weighted choice
+def unique_weight(population, weights=None, k=1):
+    if k > len(population):
+        raise ValueError("sample size cannot be larger than population")
+    is_running = True
+    while is_running:
+        non_unique = random.choices(population, weights, k)
+        unique = []
+        for value in non_unique:
+            if value in unique:
+                continue
+            unique.append(value)
+        if len(unique) < len(non_unique):
+            continue
+        is_running = False
+    return unique
+
+# Shuffle
+def shuffle(objects):
+    if type(objects) == list:
+        random.shuffle(objects)
+        return objects
+    elif type(objects) == dict:
+        dictionary = list(objects.items())
+        random.shuffle(dictionary)
+        objects = dict(dictionary)
+        return objects
+    else:
+        raise ValueError("this function was not designed for data types that aren't lists and dictionaries")
